0fc8993a6d5e6d93dc188427d8a29dca761a3845
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-03-15 15:51:18 +0000

    cucumber setup

---
+++ features/menu.feature
@@ -0,0 +1,9 @@
+Feature: Showing the menu
+As a coffee shop worker
+I want to show the menu
+So my customers can choose an item
+
+Scenario: Showing the menu
+Given I want a coffee
+And I go to the coffee shop
+Then I want to see the menu
---
+++ features/step_definitions.rb
@@ -0,0 +1,11 @@
+Given(/^I want a coffee$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I go to the coffee shop$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I want to see the menu$/) do
+  pending # express the regexp above with the code you wish you had
+end
