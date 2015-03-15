13c8a25ffbc7195bfa47c12fbaa227f61bb0765d
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2015-03-15 18:34:48 +0000

    displaying items method and test passing

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,10 +1,15 @@
 require 'menu'
 
 describe 'menu' do
+
+  let (:menu) {Menu.new}
   
-  it 'should display the price list from the json file' do
-    menu = Menu.new
+  it 'should read the prices from the json file' do
     expect(menu.price_list).to be_instance_of Hash
+    expect(menu.price_list).not_to be_empty
   end
 
+  it 'should be able to display an item' do
+    expect(menu.price_list.keys).to include('Tea')
+  end
 end
